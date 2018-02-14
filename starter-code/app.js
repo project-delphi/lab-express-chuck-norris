44044ddc7b194ca3f7dff44ace8c03ea7da8a3e8
Ravi Kalia <ravkalia@gmail.com>
2018-02-14 13:12:32 -0600

    iteration 1 done

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,5 +1,45 @@
-const express = require('express');
-const app = express();
-const Chuck  = require('chucknorris-io');
-const client = new Chuck();
+const express = require('express')
+const app = express()
+var path = require('path');
+const Chuck  = require('chucknorris-io')
+const client = new Chuck()
 
+app.set('view engine', 'pug')
+app.set('views', path.join(__dirname, 'views'))
+
+app.get('/', (req, res) => {
+  res.render('layout', {
+  })
+})
+
+
+app.get('/random', (req, res) => {
+// Retrieve a random chuck joke
+  client.getRandomJoke()
+    .then((response) => {
+      // use the response here
+      res.render('joke', { joke : response.value
+      })
+    }).catch((err) => {
+      console.log(err)
+    })
+})
+
+app.get('/categories', (req, res) => {
+  client.getJokeCategories()
+    .then((response)=>  {
+      // use the response here
+      res.render('categories', { categories: response
+      })
+    })
+    .catch((err)=> {
+      // handle error
+    })
+})
+
+app.get('/search', (req, res) => {
+  res.render('', {
+  })
+})
+
+app.listen('3000','localhost',()=>{console.log('listening on 3000')})
